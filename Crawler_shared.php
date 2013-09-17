30410a619b2619565e1ef53faafb1dd8b4513e54
ewdchn <ewdchn@gmail.com>
2013-09-17 23:00:17 +0800

    Status:  All L0 results fetched, as saved in page[page_number].html
    modified:main.php, Crawler_L0.php,Crawler_shared.php

---
+++ Crawler_shared.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ewdchn
+ * Date: 9/17/13
+ * Time: 10:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Crawler;
+
+
+class Crawler_shared {
+
+}
---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ewdchn
+ * Date: 9/17/13
+ * Time: 5:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
