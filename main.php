31d9b21ceb479fb87868ffb66f9f5a579f3bf12f
ewdchn <ewdchn@gmail.com>
2013-10-05 17:28:57 +0800

    modified: parseEntries. track error entry pages

--- main.php
+++ main.php
@@ -18,12 +18,15 @@ else
 
 define('resultCnt', 522);
 define('TmpDir', "tmp");
-
+$missing = array();
 $L0eids = array();
 $L0Data = array();
 $L1eids = array();
 $L1Data = array();
+$childrenArr = array();
 
+
+//grab search query results
 function searchGrab()
 {
     echo "Start Crawling L0...\n";
@@ -38,6 +41,7 @@ function searchGrab()
     echo "done\n";
 }
 
+//parse search query results
 function searchParse()
 //parse each page of search results and output the eid of result entries as array
 {
@@ -52,8 +56,9 @@ function searchParse()
     return $_eidArray;
 }
 
-function grabEntries($_eidArray)
+
 //given eid array, grab the entry page and save in tmp/
+function grabEntries($_eidArray)
 {
     echo "Start Grabbing Entries...";
     foreach ($_eidArray as $eid => $key)
@@ -76,31 +81,45 @@ function grabEntries($_eidArray)
     echo "\ndone\n";
 }
 
-function parseEntries($_eidArray, &$_entryData)
 //given eid array, find them in tmp folder, parse and return Data and eids of next level citations
+function parseEntries($_eidArrayArr, &$_entryDataArr, &$_childrenArr)
 {
-    if (empty($_eidArray))
+    global $missing;
+
+    if (empty($_eidArrayArr))
         throw new \Exception("array is empty");
+
+    echo "parsing...";
     $missingCnt = 0;
-    $L2eids = array();
-    foreach ($_eidArray as $eid => $key)
+    $parentLveid = array();
+    foreach ($_eidArrayArr as $entryeid => $key)
     {
-        $fileName = TmpDir . DIR_SEP . 'entry_' . $eid . '.html';
+        echo ".";
+        $fileName = TmpDir . DIR_SEP . 'entry_' . $entryeid . '.html';
         $response = file_get_contents($fileName);
-        $_entryData[$eid] = \Parser\parseEntry($response);
-        if ($_entryData[$eid] === false)
+        $_entryDataArr[$entryeid] = \Parser\parseEntry($response);
+        // Error with Entry Page
+        if ($_entryDataArr[$entryeid] === false)
         {
-            echo " $eid\n";
+//            echo " $entryeid\n";
+            echo "\n\!";
             $missingCnt++;
+            $missing[$entryeid] = true;
             continue;
         }
-        $_entryData[$eid]['citation'] = \Parser\parseCitation($response);
 
-        foreach ($_entryData[$eid]['citation'] as $_eid)
-            $L2eids[$_eid] = true;
+        //get citations(pointer to parents)
+        $_entryDataArr[$entryeid]['citation'] = \Parser\parseCitation($response);
+
+        foreach ($_entryDataArr[$entryeid]['citation'] as $parenteid)
+        {
+            $parentLveid[$parenteid] = true;  // mark as existant next level
+            //if Entry Page error occured, find it's children (last level)
+            $_childrenArr[$parenteid][] = $entryeid;//add children to parent
+        }
     }
     echo "missing $missingCnt\n";
-    return $L2eids;
+    return $parentLveid;
 }
 
 set_time_limit(0);
@@ -114,25 +133,29 @@ file_put_contents("l0eid.txt", serialize($L0eids));
 
 //parse/extract level 0
 $L0eids = unserialize(file_get_contents("l0eid.txt"));
-echo "level 0 " . count($L0eids) . "\n";
+echo "level 0 :" . count($L0eids) . "\n";
 grabEntries($L0eids);
-$L1eids = parseEntries($L0eids, $L0Data);
+$L1eids = parseEntries($L0eids, $L0Data,$childrenArr);
 file_put_contents("l1eid.txt", serialize($L1eids));
 
 
 //L1
 $L1eids = unserialize(file_get_contents("l1eid.txt"));
-echo "level 1 " . count($L1eids) . "\n";
+echo "level 1 :" . count($L1eids) . "\n";
 grabEntries($L1eids);
-$L2eids = parseEntries($L1eids, $L1Data);
+$L2eids = parseEntries($L1eids, $L1Data,$childrenArr);
 file_put_contents("l2eid.txt", serialize($L2eids));
 
 
 //L2
 $L2eids = unserialize(file_get_contents("l2eid.txt"));
-echo "level 2 " . count($L2eids) . "\n";
+echo "level 2 :" . count($L2eids) . "\n";
 grabEntries($L2eids);
 
+foreach($missing as $eid=>$key){
+    $key = $childrenArr[$eid];
+}
+file_put_contents("missing.txt",serialize($missing));
 //echo "\n",count($level0eids);
 //foreach($level0eids as $eid){
 //    echo level0_grabPaperEntry($eid);
